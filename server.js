f01e9c2257ce21601cedee414b8800aff6288154
Parvarish Bedi <parvarishbedi@Parvarishs-MBP.lan>
2024-05-18 15:02:20 +0530

    craft member create route

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const Member = require('./models/member');
 
 
 //Basic Route
 app.get('/',(req,res) => {
     res.send('Hello from Express Server');
+});
+
+//Create ( POST )
+
+app.post('/members',async(req,res) => {
+    try {
+        const newMember = new Member(req.body);
+        const savedMember = await newMember.save();
+        res.status(201).json(savedMember);
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
 });
 
 //start the server
