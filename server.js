bd859a3d3bf39cccdcaee76ea86482016434f068
Parvarish Bedi <parvarishbedi@Parvarishs-MBP.lan>
2024-05-18 15:51:02 +0530

    complete RESTFUL CRUD API for member model

--- server.js
+++ server.js
@@ -31,6 +31,44 @@ app.get('/members',async(req,res) => {
     }
 });
 
+//Read ( GET - a single member )
+app.get('/members/:id',async (req,res) => {
+    try {
+        const member = await Member.findById(req.params.id);
+        if(!member){
+            return res.status(404).json({error:'Member not found'});
+        }
+        res.json(member);
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+});
+
+//Update (PUT)
+app.put('/member/:id',async(req,res) => {
+    try {
+        const updatedMember = await Member.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!updatedMember){
+            return res.status(404).json({ error:"Member not found"});
+        }
+        res.json(updatedMember);
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+});
+
+//DELETE ( DELETE )
+app.delete('/members/:id',async(req,res) => {
+    try {
+        const deletedMember = await Member.findByIdAndDelete(req.params.id);
+        if(!deletedMember){
+            return res.status(404).json({error:"Member not found"})
+        }
+        res.json({message:"Member Deleted"})
+    } catch (error) {
+        res.status(500).json({ error:error.message});
+    }
+})
 
 //start the server
 app.listen(PORT,() => {
