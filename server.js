bbd0c928f554ab205f1c2abc172f87b2eac2491c
Parvarish Bedi <parvarishbedi@Parvarishs-MacBook-Pro.local>
2024-05-21 22:51:59 +0530

    install and use cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
 const Member = require('./models/member');
+
+app.use(cors());// Enable cors for all routes
 
 
 //Basic Route
